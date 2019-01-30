05b817e0bfe5d390bd399cee9d82ae94c35d155b
Noah Kantrowitz <noah@coderanger.net>
2019-01-29 20:06:30 -0800

    Add shell, pyshell, and dbshell commands.

---
+++ pkg/cmd/dbshell.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 Ridecell, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Ridecell/ridectl/pkg/exec"
+	"github.com/Ridecell/ridectl/pkg/kubernetes"
+)
+
+func init() {
+	rootCmd.AddCommand(dbShellCmd)
+}
+
+var dbShellCmd = &cobra.Command{
+	Use:   "dbshell [flags] <cluster_name>",
+	Short: "Open a database shell on a Summon instance",
+	Long:  `Open an interactive PostgreSQL shell for a Summon instance running on Kubernetes`,
+	Args: func(_ *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("Cluster name argument is required")
+		}
+		if len(args) > 1 {
+			return fmt.Errorf("Too many arguments")
+		}
+		return nil
+	},
+	Run: func(_ *cobra.Command, args []string) {
+		clientset, err := kubernetes.GetClient(kubeconfigFlag)
+		if err != nil {
+			panic(nil)
+		}
+		pod, err := kubernetes.FindSummonPod(clientset, args[0], fmt.Sprintf("team=%s,application=spilo", args[0]))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Connecting to %s/%s\n", pod.Namespace, pod.Name)
+
+		// Spawn kubectl exec.
+		kubectlArgs := []string{"kubectl", "exec", "-it", "-n", pod.Namespace, pod.Name, "--", "bash", "-l", "-c", "psql -U summon summon"}
+		err = exec.Exec(kubectlArgs)
+		// If we get this far, something is very wrong.
+		panic(err)
+	},
+}
---
+++ pkg/cmd/pyshell.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 Ridecell, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Ridecell/ridectl/pkg/exec"
+	"github.com/Ridecell/ridectl/pkg/kubernetes"
+)
+
+func init() {
+	rootCmd.AddCommand(pyShellCmd)
+}
+
+var pyShellCmd = &cobra.Command{
+	Use:   "pyshell [flags] <cluster_name>",
+	Short: "Open a Python shell on a Summon instance",
+	Long:  `Open an interactive Python terminal on a Summon instance running on Kubernetes`,
+	Args: func(_ *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("Cluster name argument is required")
+		}
+		if len(args) > 1 {
+			return fmt.Errorf("Too many arguments")
+		}
+		return nil
+	},
+	Run: func(_ *cobra.Command, args []string) {
+		clientset, err := kubernetes.GetClient(kubeconfigFlag)
+		if err != nil {
+			panic(nil)
+		}
+		pod, err := kubernetes.FindSummonPod(clientset, args[0], fmt.Sprintf("app.kubernetes.io/instance=%s-web", args[0]))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Connecting to %s/%s\n", pod.Namespace, pod.Name)
+
+		// Spawn kubectl exec.
+		kubectlArgs := []string{"kubectl", "exec", "-it", "-n", pod.Namespace, pod.Name, "--", "bash", "-l", "-c", "python manage.py shell"}
+		err = exec.Exec(kubectlArgs)
+		// If we get this far, something is very wrong.
+		panic(err)
+	},
+}
