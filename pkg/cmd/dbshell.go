ba59917d368e6a6c1fea3b17c1f02f0f6fffd0af
dhouti <l.rex.via@gmail.com>
2019-03-20 16:01:12 -0700

    initial commit of new dbshell behavior

--- pkg/cmd/dbshell.go
+++ pkg/cmd/dbshell.go
@@ -18,6 +18,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -44,18 +45,62 @@ var dbShellCmd = &cobra.Command{
 		return nil
 	},
 	RunE: func(_ *cobra.Command, args []string) error {
+
+		namespace := strings.Split(args[0], "-")[1]
+
 		clientset, err := kubernetes.GetClient(kubeconfigFlag)
 		if err != nil {
 			return errors.Wrap(err, "unable to load Kubernetes configuration")
 		}
-		pod, err := kubernetes.FindSummonPod(clientset, args[0], fmt.Sprintf("team=%s,application=spilo", args[0]))
+
+		// dynamicClient is needed to retrieve custom resources (for now)
+		dynamicClient, err := kubernetes.GetDynamicClient(kubeconfigFlag)
+		if err != nil {
+			return err
+		}
+		// Retrieve our SummonPlatform object for specified cluster
+		summonObject, err := kubernetes.FindSummonObject(dynamicClient, args[0])
+		if err != nil {
+			return err
+		}
+
+		// Check if datbaseSpec exists in our summon object
+		var exclusiveDatabase bool
+		var sharedDatabaseName string
+		// Messy hack to grab just the things we care about
+		databaseSpec, ok := summonObject.Object["spec"].(map[string]interface{})["database"].(map[string]interface{})
+		if ok {
+			exclusiveDatabase, ok = databaseSpec["exclusiveDatabase"].(bool)
+			if !ok {
+				exclusiveDatabase = false
+			}
+			sharedDatabaseName, ok = databaseSpec["sharedDatabaseName"].(string)
+			if !ok {
+				sharedDatabaseName = namespace
+			}
+		} else {
+			exclusiveDatabase = false
+			sharedDatabaseName = namespace
+		}
+
+		if exclusiveDatabase {
+			pod, err := kubernetes.FindSummonPod(clientset, args[0], "application=spilo,spilo-role=master")
+			if err != nil {
+				return errors.Wrap(err, "unable to find pod")
+			}
+			fmt.Printf("Connecting to %s/%s\n", pod.Namespace, pod.Name)
+			// Spawn kubectl exec.
+			kubectlArgs := []string{"kubectl", "exec", "-it", "-n", pod.Namespace, pod.Name, "--", "bash", "-l", "-c", "psql -U summon summon"}
+			return exec.Exec(kubectlArgs)
+		}
+
+		// If we got here we're using a shared database
+		pod, err := kubernetes.GetPod(clientset, namespace, fmt.Sprintf(`^%s-database-[0-9]+`, sharedDatabaseName), "application=spilo,spilo-role=master")
 		if err != nil {
 			return errors.Wrap(err, "unable to find pod")
 		}
-		fmt.Printf("Connecting to %s/%s\n", pod.Namespace, pod.Name)
-
-		// Spawn kubectl exec.
-		kubectlArgs := []string{"kubectl", "exec", "-it", "-n", pod.Namespace, pod.Name, "--", "bash", "-l", "-c", "psql -U summon summon"}
+		dbUsername := strings.Replace(args[0], "-", "_", -1)
+		kubectlArgs := []string{"kubectl", "exec", "-it", "-n", pod.Namespace, pod.Name, "--", "bash", "-c", fmt.Sprintf("psql -U %s", dbUsername)}
 		return exec.Exec(kubectlArgs)
 	},
 }
