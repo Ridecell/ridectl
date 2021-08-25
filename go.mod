57e8495b64e2efd89ccd04672ea80bbfe202c52c
shubhindia <shubhindia123@gmail.com>
2021-08-25 12:56:51 +0530

    Initial ridectl cmd skeleton
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Ridecell/ridectl
+
+go 1.16
+
+require (
+	github.com/Ridecell/summon-operator v0.0.0-20210824064123-59285fb3b472
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.2.1
+	k8s.io/apimachinery v0.22.1
+	k8s.io/client-go v0.22.1
+)
---
+++ pkg/cmd/root.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2019 Ridecell, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	summonv1beta2 "github.com/Ridecell/summon-operator/apis/app/v1beta2"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	hackapis "github.com/Ridecell/ridectl/pkg/apis"
+)
+
+var kubeconfigFlag string
+var versionFlag bool
+var version string
+
+var rootCmd = &cobra.Command{
+	Use:   "ridectl",
+	Short: "Ridectl controls Summon instances in Kubernetes",
+	RunE: func(_ *cobra.Command, args []string) error {
+		if versionFlag {
+			fmt.Printf("ridectl version %s\n", version)
+		}
+		return nil
+	},
+}
+
+func init() {
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+	rootCmd.PersistentFlags().StringVar(&kubeconfigFlag, "kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	rootCmd.Flags().BoolVar(&versionFlag, "version", true, "--version")
+	// Register all types from ridecell-operator.
+	summonv1beta2.AddToScheme(scheme.Scheme)
+	hackapis.AddToScheme(scheme.Scheme)
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
