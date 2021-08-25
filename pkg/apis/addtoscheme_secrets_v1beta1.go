e0861b651d5804c42376c3cb6796b45cfe048861
shubhindia <shubhindia123@gmail.com>
2021-08-25 13:55:32 +0530

    add secrets apis
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- pkg/apis/addtoscheme_secrets_v1beta1.go
+++ pkg/apis/addtoscheme_secrets_v1beta1.go
@@ -1,9 +1,12 @@
 /*
-Copyright 2021 Ridecell, Inc.
+Copyright 2021 Ridecell, Inc..
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -11,12 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package apis
 
 import (
-	"github.com/Ridecell/ridectl/pkg/cmd"
+	"github.com/Ridecell/ridectl/pkg/apis/secrets/v1beta2"
 )
 
-func main() {
-	cmd.Execute()
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1beta2.SchemeBuilder.AddToScheme)
 }
