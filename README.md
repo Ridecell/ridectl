e3065132103080e65a92fccaf0aea74b438d2bb9
Shubham Gopale <shubhindia123@gmail.com>
2021-10-04 11:52:14 +0530

    Devops-2925 ridectl restart (#89)

--- README.md
+++ README.md
@@ -1,16 +1,55 @@
 # Ridectl
 
 ## Overview
-`ridectl` is Ridecell's internal tool that enables employees access and ways to interact with SummonPlatform instances. Employee's permissions are restricted to certain environments or features, depending on their role.
+`ridectl` is Ridecell's internal tool that enables employees access and ways to interact with SummonPlatform/Microservice instances. Employee's permissions are restricted to certain environments or features, depending on their role.
 
 Some key features are:
-1. shelling into an instance (`shell`)
-2. shelling into an instance under the python environment (`pyshell`)
-3. shelling into the instance's database (`dbshell`)
-4. Obtaining dispatcher/support/reports account password (`password`)
-5. Restart migrations for a summon instance(`restart-migrations`)
-6. Create new summon-platform instance yml (`new`)
-
+1. Shelling into an instance (`shell`)\
+    a. Summon-platform
+    ```
+    ridectl shell summontest-dev
+    ```
+    b. Microservice
+    ```
+    ridectl shell svc-us-master-webhook-sms
+    ```
+2. Shelling into an instance under the python environment (`pyshell`)\
+    a. Summon-platform
+    ```
+    ridectl pyshell summontest-dev
+    ```
+    b. Microservice
+    ```
+    ridectl pyshell svc-us-master-webhook-sms
+    ```
+3. Shelling into the instance's database (`dbshell`)\
+    a. Summon-platform
+    ```
+    ridectl dbshell summontest-dev
+    ```
+    b. Microservice
+    ```
+    ridectl dbshell svc-us-master-webhook-sms
+    ```
+4. Obtaining dispatcher account password (`password`)\
+    a. Summon-platform
+    ```
+    ridectl password summontest-dev
+    ```
+5. Restart migrations for a summon instance(`restart-migrations`)\
+    a. Summon-platform
+    ```
+    ridectl restart-migrations summontest-dev
+    ```
+6. Restart all pods of a certain type (web|celeryd|etc) (`restart`)\
+    a. Summon-platform
+    ```
+    ridectl restart summontest-dev web
+    ```
+    b. Microservice
+    ```
+    ridectl restart svc-us-master-webhook-sms web
+    ```
 For a full list of functionalities, run `ridectl --help`
 
 ## Installing `ridectl`
--- pkg/cmd/restart.go
+++ pkg/cmd/restart.go
@@ -17,26 +17,42 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"reflect"
+	"time"
 
-	"github.com/Ridecell/ridectl/pkg/exec"
 	"github.com/Ridecell/ridectl/pkg/kubernetes"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	utils "github.com/Ridecell/ridectl/pkg/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func init() {
 	rootCmd.AddCommand(rollingRestartCmd)
 }
 
+/*
+We are using summon-operator to create deployments of summon-platform. So when we try to do rollout restart,
+it does not behave as expected because summon-operator is constantly watching the deployments and reconciles if anything changes.
+So when we do rollout restart k8s starts a new pod but it gets terminated immediately because summon-operator restarts.
+This becomes very tricky to handle in operator-itself. Hence we have implemented another way to do this
+Ref: https://ridecell.atlassian.net/browse/DEVOPS-2925
+*/
 var rollingRestartCmd = &cobra.Command{
 	Use:   "restart [flags] <cluster_name> <pod_type>",
 	Short: "Performs a rolling restart of pods.",
 	Long: "Restarts all pods of a certain type (web|celeryd|etc).\n" +
-		"restart <instance> <deployment> e.g ridectl restart summontest-dev web",
+		"For summon instances: restart <tenant>-<env> <type>                 -- e.g. ridectl restart summontest-dev web\n" +
+		"For microservices: restart svc-<region>-<env>-<microservice> <type>  -- e.g. ridectl svc-us-master-webhook-sms web",
 	Args: func(_ *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			return fmt.Errorf("Cluster name argument is required.")
@@ -54,6 +70,7 @@ var rollingRestartCmd = &cobra.Command{
 		if !binaryExists {
 			return fmt.Errorf("kubectl is not installed. Follow the instructions here: https://kubernetes.io/docs/tasks/tools/#kubectl to install it")
 		}
+		fmt.Printf("\nWarning: This might cause downtime for your services\n")
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -63,12 +80,72 @@ var rollingRestartCmd = &cobra.Command{
 			return errors.Wrapf(err, "not a valid target %s", args[0])
 		}
 
+		var deploymentName string
+		podLabels := make(map[string]string)
+
+		if target.Type == "summon" {
+			podLabels["app.kubernetes.io/instance"] = fmt.Sprintf("%s-web", args[0])
+			deploymentName = fmt.Sprintf("%s-%s", args[0], args[1])
+		} else if target.Type == "microservice" {
+			podLabels["app"] = fmt.Sprintf("%s-svc-%s", target.Env, target.Namespace)
+			podLabels["environment"] = target.Env
+			podLabels["region"] = target.Region
+			podLabels["role"] = args[1]
+			deploymentName = fmt.Sprintf("%s-svc-%s-%s", target.Env, target.Namespace, args[1])
+		}
+
 		kubeObj := kubernetes.GetAppropriateObjectWithContext(*kubeconfig, args[0], target)
 		if reflect.DeepEqual(kubeObj, kubernetes.Kubeobject{}) {
 			return errors.Wrapf(err, "no instance found %s", args[0])
 		}
 
-		kubectlArgs := []string{"kubectl", "rollout", "restart", "deployment", "-n", target.Namespace, fmt.Sprintf("%s-%s", target.Name, args[1]), "--context", kubeObj.Context.Cluster}
-		return exec.Exec(kubectlArgs)
+		labelSet := labels.Set{}
+		for k, v := range podLabels {
+			labelSet[k] = v
+		}
+
+		listOptions := &client.ListOptions{
+			Namespace:     target.Namespace,
+			LabelSelector: labels.SelectorFromSet(labelSet),
+		}
+
+		pods := &corev1.PodList{}
+		err = kubeObj.Client.List(context.TODO(), pods, listOptions)
+		if err != nil {
+			return errors.Wrap(err, "error listing pods")
+		}
+
+		deployment := &appsv1.Deployment{}
+
+		var restartSuccess bool
+		for !restartSuccess {
+			for _, pod := range pods.Items {
+				err = kubeObj.Client.Delete(context.TODO(), &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      pod.Name,
+						Namespace: target.Namespace,
+					},
+				})
+				if err != nil {
+					return errors.Wrap(err, "error deleting pod")
+				}
+
+				// waiting for the deployment to be ready
+				err = wait.Poll(time.Second*5, time.Minute*3, func() (bool, error) {
+					_ = kubeObj.Client.Get(context.TODO(), types.NamespacedName{Name: deploymentName, Namespace: target.Namespace}, deployment)
+
+					if deployment.Status.ReadyReplicas == deployment.Status.Replicas {
+						return true, nil
+					}
+					return false, nil
+				})
+				if err != nil {
+					return errors.Wrap(err, "error waiting for deployment to re ready")
+				}
+			}
+			restartSuccess = true
+		}
+		fmt.Printf("\nSuccessfully restarted pods for %s : %s\n", args[0], args[1])
+		return nil
 	},
 }
