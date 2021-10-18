2c44bef2d78e3fc16c082bf6229b6dc36145a0be
shubhindia <shubhindia123@gmail.com>
2021-10-18 12:44:24 +0530

    use HEAD instead of GET
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -40,7 +40,7 @@ func CheckBinary(binary string) bool {
 }
 
 func CheckVPN() {
-	resp, err := http.Get("https://ridectl.s3.us-west-2.amazonaws.com/machinload01.png")
+	resp, err := http.Head("https://ridectl.s3.us-west-2.amazonaws.com/machinload01.png")
 	if err != nil {
 		fmt.Println("\n", err)
 	}
