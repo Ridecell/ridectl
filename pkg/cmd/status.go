efdd128d2deea8e05d136e74855eb23a7e88819b
Kunal Gohire <37223681+kunalp-gohire@users.noreply.github.com>
2021-12-01 10:03:56 +0530

    DEVOPS-2915 add postgresdump status (#100)

--- pkg/cmd/status.go
+++ pkg/cmd/status.go
@@ -17,12 +17,14 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
 	"os"
 	osExec "os/exec"
 	"reflect"
+	"strings"
+	"time"
 
 	"github.com/Ridecell/ridectl/pkg/utils"
+	"github.com/manifoldco/promptui"
 	"github.com/pkg/errors"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
@@ -65,81 +67,134 @@ func getData(objType string, context string, namespace string, tenant string) (s
 		if err != nil {
 			return "", errors.Wrap(err, "error getting deployment info")
 		}
+	} else {
+		objectData, err := TempFS.ReadFile("templates/show_postgresdump.tpl")
+		if err != nil {
+			return "", errors.Wrap(err, "error reading show_postgresdump.tpl")
+		}
+		command := "kubectl get postgresdumps.db.controllers.ridecell.io -n " + namespace + " --context " + context + " -o" + " go-template='" + string(objectData) + "'"
+		if strings.Contains(tenant, "svc-") {
+			command = "kubectl get postgresdumps.db.controllers.ridecell.io -n " + namespace + " --context " + context + " -o" + " go-template='" + string(objectData) + "'" + " | grep " + tenant
+		}
+		data, err = osExec.Command("bash", "-c", command).Output()
+		if err != nil {
+			return "", errors.Wrap(err, "error getting postgresdump instance info")
+		}
 	}
 
 	return string(data), err
 }
 
 var statusCmd = &cobra.Command{
-	Use:   "status [follow] <cluster_name>",
+	Use:   "status",
 	Short: "Get status report of an Summon Instance",
 	Long:  "Shows status details for all components of a Summon Instance",
 	Args: func(_ *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			return fmt.Errorf("cluster name argument is required")
-		}
-		if len(args) > 1 {
-			return fmt.Errorf("too many arguments")
-		}
 		return nil
 	},
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-
 		utils.CheckVPN()
 		utils.CheckKubectl()
 		return nil
 	},
-	RunE: func(_ *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		followStatus, _ := cmd.Flags().GetBool("follow")
+		statusTypes := []string{"Summon Platform", "DB Backup"}
+		statusPrompt := promptui.Select{
+			Label: "Select ",
+			Items: statusTypes,
+		}
+		_, statusType, err := statusPrompt.Run()
+		if err != nil {
+			return errors.Wrapf(err, "Prompt failed")
+		}
+		validator := func(input string) error {
+			if input == "" {
+				return errors.New("Invalid summont tenant name or microservice name")
+			}
+			return nil
+		}
+		instanceNamePromt := promptui.Prompt{
+			Label:    "Enter summon tenant(sandbox-dev)/microservice(svc-us-master-microservice) name",
+			Validate: validator,
+		}
+		name, err := instanceNamePromt.Run()
+		if err != nil {
+			return errors.Wrapf(err, "Prompt failed")
+		}
+
 		kubeconfig := utils.GetKubeconfig()
-		target, err := kubernetes.ParseSubject(args[0])
+		target, err := kubernetes.ParseSubject(name)
 		if err != nil {
 			pterm.Error.Println(err, "Its not a valid Summonplatform or Microservice")
 			os.Exit(1)
 		}
 
-		kubeObj := kubernetes.GetAppropriateObjectWithContext(*kubeconfig, args[0], target, inCluster)
+		kubeObj := kubernetes.GetAppropriateObjectWithContext(*kubeconfig, name, target, inCluster)
 		if reflect.DeepEqual(kubeObj, kubernetes.Kubeobject{}) {
-			pterm.Error.Printf("No instance found %s\n", args[0])
+			pterm.Error.Printf("No instance found %s\n", name)
 			os.Exit(1)
 		}
 
-		sData, err := getData("summon", kubeObj.Context.Cluster, target.Namespace, args[0])
-		if err != nil {
-			return err
-		}
-		dData, err := getData("deployment", kubeObj.Context.Cluster, target.Namespace, args[0])
-		if err != nil {
-			return err
-		}
-		if follow {
-			area, _ := pterm.DefaultArea.WithRemoveWhenDone().Start()
-
-			for {
-				area.Update(sData, "\n", dData)
-				p := pterm.DefaultProgressbar.WithTotal(2)
-				p.ShowElapsedTime = false
-				p.RemoveWhenDone = true
-				_, _ = p.Start()
-				p.Title = "Fetching data"
-
-				// Calling it at end of for loop since we made these calls right before.
-				sData, err = getData("summon", kubeObj.Context.Cluster, target.Namespace, args[0])
-				if err != nil {
-					return err
-				}
-				p.Increment()
-
-				dData, err = getData("deployment", kubeObj.Context.Cluster, target.Namespace, args[0])
-				if err != nil {
-					return err
-				}
-				p.Increment()
-				_, _ = p.Stop()
-				_ = area.Stop()
-
+		var sData, dData, pData string
+		if statusType == "Summon Platform" {
+			sData, err = getData("summon", kubeObj.Context.Cluster, target.Namespace, name)
+			if err != nil {
+				return err
+			}
+			dData, err = getData("deployment", kubeObj.Context.Cluster, target.Namespace, name)
+			if err != nil {
+				return err
 			}
 		} else {
-			pterm.Success.Printf(sData + "\n" + dData + "\n")
+			pData, err = getData("postgresdump", kubeObj.Context.Cluster, target.Namespace, name)
+			if err != nil {
+				return err
+			}
+		}
+		if followStatus {
+			area, _ := pterm.DefaultArea.WithRemoveWhenDone().Start()
+			for {
+				p := pterm.DefaultProgressbar
+				p.ShowElapsedTime = false
+				p.RemoveWhenDone = true
+				if statusType == "Summon Platform" {
+					p = *p.WithTotal(2)
+					area.Update(sData, "\n", dData)
+					_, _ = p.Start()
+					p.Title = "Fetching data"
+					sData, err = getData("summon", kubeObj.Context.Cluster, target.Namespace, name)
+					if err != nil {
+						return err
+					}
+					p.Increment()
+
+					dData, err = getData("deployment", kubeObj.Context.Cluster, target.Namespace, name)
+					if err != nil {
+						return err
+					}
+					p.Increment()
+				} else {
+					p = *p.WithTotal(1)
+					area.Update(pData)
+					_, _ = p.Start()
+					p.Title = "Fetching data"
+					pData, err = getData("posgtresdump", kubeObj.Context.Cluster, target.Namespace, name)
+					if err != nil {
+						return err
+					}
+					p.Increment()
+					time.Sleep(time.Second * 10)
+				}
+				_, _ = p.Stop()
+				_ = area.Stop()
+			}
+		} else {
+			if statusType == "Summon Platform" {
+				pterm.Success.Printf(sData + "\n" + dData)
+			} else {
+				pterm.Success.Printf(pData)
+			}
 		}
 		return nil
 	},
--- pkg/cmd/templates/show_postgresdump.tpl
+++ pkg/cmd/templates/show_postgresdump.tpl
@@ -1,4 +1,6 @@
-STATUS: {{.status.status  }}
-MESSAGE: {{.status.message -}}
-{{if .status.dumpPath  }}
-S3PATH: {{.status.dumpPath }}{{end}}
+{{printf "%-70s%-15s%-50s" "NAME" "STATUS" "MESSAGE" -}}
+{{range $postgresDump := .items }}
+{{printf "%-70s" $postgresDump.metadata.name -}}
+{{printf "%-15s" $postgresDump.status.status -}}
+{{printf "%-50s" $postgresDump.status.message -}}
+{{end -}} 
