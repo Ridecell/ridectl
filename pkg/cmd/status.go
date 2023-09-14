499bd7f63c61e826f11078e385b5e79e04a06460
Gaurang Kudale <gaurangkudale@yahoo.com>
2023-09-14 11:39:34 +0530

    formatting code in status.go

--- pkg/cmd/status.go
+++ pkg/cmd/status.go
@@ -123,7 +123,7 @@ var statusCmd = &cobra.Command{
 		}
 		
 		// Trimming the starting and ending white-spaces from user input string
-        name = utils.TrimWhiteSpaces(name)
+        	name = utils.TrimWhiteSpaces(name)
 		target, kubeObj, exist := utils.DoesInstanceExist(name, inCluster)
 		if !exist {
 			os.Exit(1)
